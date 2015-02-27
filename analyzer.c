406f48e6fad721077404fd5a5a87abd2e475b100
Hari K <hari01234@gmail.com>
2015-02-27 18:26:04 +0530

    Changed the numerical-constants recognition code to use regular expressions

--- analyzer.c
+++ analyzer.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<regex.h>
 
 int main(int argc, char **argv)
 {
+    regex_t intg,flt,expnz;
+    regmatch_t _intg[1],_flt[1], _expn[1];
     FILE *fp = fopen(argv[1],"r");
-    char *token = NULL , c;
-    int i = 0;
+    char *token = NULL, *buf = NULL, *_match = NULL, c;
+    int i = 0, j, w = 0, begin, end, size, integer, fl, exp;
     size_t len = 0;
+
+    regcomp(&intg, "([[:digit:]]+)", REG_EXTENDED|REG_NEWLINE);
+    regcomp(&flt, "([[:digit:]]+.[[:digit:]]+)", REG_EXTENDED|REG_NEWLINE);
+    regcomp(&expnz, "([[:digit:]]+.[[:digit:]]+[Ee][+-]?[[:digit:]]+)", REG_EXTENDED|REG_NEWLINE);
 
     while((getline(&token, &len, fp))!=-1)
     {
@@ -73,20 +80,63 @@ int main(int argc, char **argv)
                 {
                     if(isdigit(c))
                     {
-                        printf("<number,");
-                        while(isdigit(c))
+                        exp = regexec(&expnz, &token[i-1], 1, _expn, 0);
+                        if(!exp)
                         {
-                            printf("%c",c);
-                            c = token[i++];
+                            begin =  (int)_expn[0].rm_so;
+                            end = (int)_expn[0].rm_eo;
+                            size = end - begin;
+                            _match = malloc(size+1);
+                            for(j=begin;j<end;++j)
+                            {
+                                _match[w++] = token[i-1+j];
+                            }
+                            i = i + size-1;
+                            _match[w] = '\0';
+                            w = 0;
+                            printf("<exponential-constant,%s>",_match);
+                            break;
                         }
-                        printf(">");
-                        --i;
+                        fl = regexec(&flt, &token[i-1], 1, _flt, 0);
+                        if(!fl)
+                        {
+                            begin =  (int)_flt[0].rm_so;
+                            end = (int)_flt[0].rm_eo;
+                            size = end - begin;
+                            _match = malloc(size+1);
+                            for(j=begin;j<end;++j)
+                            {
+                                _match[w++] = token[i-1+j];
+                            }
+                            i = i + size-1;
+                            _match[w] = '\0';
+                            w = 0;
+                            printf("<floating-constant,%s>",_match);
+                            break;
+                        }
+                        integer = regexec(&intg, &token[i-1], 1, _intg, 0);
+                        if(!integer)
+                        {
+                            begin = (int)_intg[0].rm_so;
+                            end = (int)_intg[0].rm_eo;
+                            size = end - begin;
+                            _match = malloc(size+1);
+                            for(j=begin;j<end;++j)
+                            {
+                                _match[w++] = token[i-1+j];
+                            }
+                            i = i + size-1;
+                            _match[w] = '\0';
+                            w = 0;
+                            printf("<integer-constant,%s>",_match);
+                            break;
+                        }
                     }
                 }
+
             }
         }
         printf("\n");
     }
-
     return 0;
 }
